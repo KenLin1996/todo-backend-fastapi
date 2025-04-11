b0327821e0eeeb4d212be69f566d630b484e9ce8
Ken Lin <jesus0910727994@gmail.com>
2025-04-12 00:39:44 +0800

    chore(api): enable CORS for all origins to allow frontend integration

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # CORS 設定：允許的前端網址
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['http://localhost:3000'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
